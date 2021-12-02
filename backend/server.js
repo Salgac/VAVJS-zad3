8f97b5c61b26ba6370ba8b904b3c6a2c60ef3675
Dominik <domco.salgovic@gmail.com>
2021-12-02 17:46:41 +0100

    Implement orders and customer api endpoints

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const app = require('express')();
+const bodyParser = require('body-parser');
 
 // database
 class Product {
@@ -11,21 +12,21 @@ class Product {
 }
 
 class Customer {
-	constructor() {
-		this.id;
-		this.email;
-		this.name;
-		this.address;
-		this.city;
-		this.postcode;
+	constructor(id, email, name, address, city, postcode) {
+		this.id = id;
+		this.email = email;
+		this.name = name;
+		this.address = address;
+		this.city = city;
+		this.postcode = postcode;
 	}
 }
 
 class Order {
-	constructor() {
-		this.customer;
-		this.products;
-		this.state;
+	constructor(customer, products) {
+		this.customer = customer;
+		this.products = products;
+		this.state = "Created";
 	}
 }
 
@@ -44,9 +45,9 @@ var advert;
 
 function initDatabase() {
 	// 3 products
-	products.push(new Product(1, "Kábel", "kabel.jpg", 4.99));
-	products.push(new Product(2, "Slúchadlá", "phones.jpg", 12.59));
-	products.push(new Product(3, "Telefon", "phone.jpg", 39.99));
+	products.push(new Product(0, "Kábel", "kabel.jpg", 4.99));
+	products.push(new Product(1, "Slúchadlá", "phones.jpg", 12.59));
+	products.push(new Product(2, "Telefon", "phone.jpg", 39.99));
 }
 
 // api
@@ -65,7 +66,7 @@ app.get('/api/products', (req, res) => {
 app.get('/api/products/:id', (req, res) => {
 	console.log("GET '/api/products/:id'");
 
-	var product = products[req.params.id - 1];
+	var product = products[req.params.id];
 	if (product != undefined)
 		res.json(product);
 	else
@@ -76,12 +77,64 @@ app.get('/api/products/:id/image', (req, res) => {
 	console.log("GET '/api/products/:id/image'");
 
 	const path = require('path');
-	var product = products[req.params.id - 1];
+	var product = products[req.params.id];
 
 	if (product != undefined)
 		res.sendFile(path.join(__dirname, "images/" + product.image));
 	else
-		res.json({ error: `Product with id ${req.params.id} does not exist.` }).status(404);
+		res.status(404).json({ error: `Product with id ${req.params.id} does not exist.` });
+});
+
+// customers
+app.get('/api/customers', (req, res) => {
+	console.log("GET '/api/customers'");
+
+	res.json({ customers: customers });
+});
+
+// order
+app.get('/api/orders', (req, res) => {
+	console.log("GET '/api/orders'");
+
+	res.json({ orders: orders });
+});
+
+app.post('/api/order', bodyParser.json(), (req, res) => {
+	console.log("POST '/api/order'");
+
+	var customer = req.body.customer;
+	var products = req.body.products;
+
+	var newCustomer = new Customer(
+		customers.length,
+		customer.email,
+		customer.name,
+		customer.address,
+		customer.city,
+		customer.postcode,
+	);
+
+	customers.push(newCustomer);
+	var newOrder = new Order(
+		newCustomer.id,
+		products,
+	)
+
+	orders.push(newOrder);
+
+	res.status(201).json({});
+});
+
+app.put('/api/order/:id', bodyParser.json(), (req, res) => {
+	console.log("PUT '/api/order/:id'");
+
+	var order = orders[req.params.id];
+	if (order != undefined) {
+		order.state = req.body.state;
+		res.json({});
+	}
+	else
+		res.status(404).json({ error: `Order with id ${req.params.id} does not exist.` });
 });
 
 // start app
